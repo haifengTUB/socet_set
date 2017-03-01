dd2ef8e6c5dabfdf727670def4dc8baa0d59bb0c
Trent Hare <thare@usgs.gov>
2017-03-01 14:27:23 -0700

    updates to latest version. pedr shapefiles now support projection and binary fields for Socet GXP colorized mapping

--- SS4HiRISE/Software/ISIS3_MACHINE/PERL_SCRIPTS/pedrTAB2SHP_og.pl
+++ SS4HiRISE/Software/ISIS3_MACHINE/PERL_SCRIPTS/pedrTAB2SHP_og.pl
@@ -10,14 +10,23 @@
 #
 #  Trent Hare, USGS
 #  Mar 2008
+#  Mar 2017, add in Mars 2000 projection and binary field mapping for ogr2ogr (*.csvt)
+#
 
 $filelist1= @ARGV[0];
 $skipline = @ARGV[1];
-#$outtxt = @ARGV[2];
+#$outcsv = @ARGV[2];
 #$outvrt = @ARGV[3];
-$outtxt = "xxtemp.csv";
-$outvrt = "xxtemp.vrt";
 
+#Create tempory name for conversion
+$root = "xxtempZ";
+$outcsv = $root.".csv";
+$outcsvt= $root.".csvt";
+$outvrt = $root.".vrt";
+$outprj = $root.".prj";
+$outshp = $root.".shp";
+$outshx = $root.".shx";
+$outdbf = $root.".dbf";
 
 chomp $skipline;
 $skipline = $skipline * 1;
@@ -25,7 +34,7 @@ if ($#ARGV != 1) {
   print "usage: pedrTAB2SHP.pl file1.tab skip_line\n";
   print "\nDescription: Convert MOLA PEDR Tab file to OGR Vrt and then Shapefile \n";
   print "     The output file name will be the input name appended with a 'Z'\n";
-  print "     The created shapefile will have a *.shp, *.shx, and *.dbf filename.\n";
+  print "     The created shapefile will have a *.shp, *.shx, *.dbf and *.prj filename.\n";
   print "\nexamples:\n";
   print "pedrTAB2SHP.pl input.tab 2\n";
   print "     For default PEDR2TAB output TAB file use a skip_line = 2\n\n";
@@ -33,15 +42,13 @@ if ($#ARGV != 1) {
 }
 
 chomp $filelist1;
-chomp $outtxt;
-chomp $outvrt;
+#chomp $outcsv;
+#chomp $outvrt;
 open FILESIN1, $filelist1;
-open OUTTXT, "> $outtxt ";
+open OUTCSV, "> $outcsv ";
+open OUTCSVT, "> $outcsvt ";
 open OUTVRT, "> $outvrt ";
 
-@fname = split('\.',$outtxt);
-$root = $fname[0];
-$outshp = $fname[0]."Z";
 @infname = split('\.',$filelist1);
 $inroot = $infname[0];
 
@@ -58,7 +65,7 @@ while (<FILESIN1>) {
     $cnt = @line * 1;
     $flg=1;
     #write out ID
-    #print OUTTXT "$iflag ";
+    #print OUTCSV "$iflag ";
     foreach $string (@line) {
         if (length($string) > 0) {
           if ($string eq "long_East") {
@@ -70,42 +77,51 @@ while (<FILESIN1>) {
             if ($lon > 180) {
                $lon = $lon - 360;
            }
-            print OUTTXT "$lon";
+            print OUTCSV "$lon";
           } else {
-            print OUTTXT "$string";
+            print OUTCSV "$string";
           }
           $flg = $flg + 1;
           if ($flg < $cnt) {
-            print OUTTXT ","
+            print OUTCSV ","
           }
         } else {
           $cnt = $cnt - 1;
         }
     }
-    print OUTTXT "\n";
+    print OUTCSV "\n";
   }
   $iflag = $iflag + 1;
 }
+close OUTCSV;
 
+#Create CSVT field mapping. Assume input for tab is
+#long_East lat_North topography MOLArange  planet_rad c A  offndr  EphemerisTime  areod_lat areoid_rad shot  pkt   orbit gm
+$fieldMapping = "\"Real\",\"Real\",\"Real\",\"Real\",\"Real\",\"Integer\",\"Integer\",\"Real\",\"String\",\"Real\",\"Real\",\"Integer\",\"Integer\",\"Integer\",\"Integer\"";
+print OUTCSVT $fieldMapping;
+close OUTCSVT;
+
+# Create ogr2ogr virtual header (*.vrt)
 print OUTVRT  "<OGRVRTDataSource>\n";
-print OUTVRT  "    <OGRVRTLayer name=\"$outshp\">\n";
-print OUTVRT  "        <SrcDataSource relativeToVRT=\"1\">.</SrcDataSource>\n";
-print OUTVRT  "        <SrcLayer>$root</SrcLayer>\n";
-print OUTVRT  "        <GeometryType>wkbPoint</GeometryType>\n";
+print OUTVRT  "    <OGRVRTLayer name=\"$root\">\n";
+#print OUTVRT  "        <SrcDataSource relativeToVRT=\"1\">.</SrcDataSource>\n";
+print OUTVRT  "        <SrcDataSource>$outcsv</SrcDataSource>\n";
+#print OUTVRT  "        <SrcDataSource relativeToVRT=\"1\">.</SrcDataSource>\n";
+#print OUTVRT  "        <SrcLayer>$root</SrcLayer>\n";
+#print OUTVRT  "        <GeometryType>wkbPoint</GeometryType>\n";
 #print OUTVRT "        <LayerSRS>WGS84</LayerSRS>\n";
 print OUTVRT  "        <GeometryField encoding=\"PointFromColumns\" x=\"long_East\" y=\"areod_lat\" z=\"topography\"/>\n";
 print OUTVRT  "    </OGRVRTLayer>\n";
 print OUTVRT  "</OGRVRTDataSource>\n";
 
-close OUTTXT;
 close OUTVRT;
 
 #Run OGR2OGR conversion
-#@args = ("ogr2ogr", "-f \"ESRI Shapefile\"", ".", "$outtxt");
-$args = "ogr2ogr -f \"ESRI Shapefile\" . $outtxt";
-if (system($args) != 0) {
-     print "system $args failed: $?\n";
-}
+#@args = ("ogr2ogr", "-f \"ESRI Shapefile\"", ".", "$outcsv");
+#$args = "ogr2ogr -f \"ESRI Shapefile\" . $outcsv";
+#if (system($args) != 0) {
+     #print "system $args failed: $?\n";
+#}
 
 #@args = ("ogr2ogr", "-lco \"SHPT=POINTZ\"", "-f \"ESRI Shapefile\"", ".", "$outvrt");
 $args = "ogr2ogr -lco SHPT=POINTZ -f \"ESRI Shapefile\" . $outvrt";
@@ -113,20 +129,23 @@ if (system($args) != 0) {
      print "system $args failed: $?\n";
 }
 
-$outshp2 = $root."Z.shp";
-$outshx2 = $root."Z.shx";
-$outdbf2 = $root."Z.dbf";
-$outdbf = $root.".dbf";
-if (-e $outshp2) {
-   $final = $inroot."Z.shp";
+
+# create projection file
+unlink ("$outprj");
+open  OUTPROJ, "> $outprj ";
+print OUTPROJ "GEOGCS[\"Mars 2000\",DATUM[\"D_Mars_2000\",SPHEROID[\"Mars_2000_IAU_IAG\",3396190.0,0.0]],PRIMEM[\"Reference_Meridian\",0.0],UNIT[\"Degree\",0.0174532925199433]]\n";
+close OUTPROJ;
+
+if (-e $outshp) {
    print " -renaming output files\n";
-   rename($outshp2, $final);
-   rename($outshx2, $inroot."Z.shx");
-   rename($outdbf2, $inroot."Z.dbf");
+   rename($outshp, $inroot."Z.shp");
+   rename($outshx, $inroot."Z.shx");
+   rename($outdbf, $inroot."Z.dbf");
+   rename($outprj, $inroot."Z.prj");
    print " -deleting temporary files\n";
-   unlink $outtxt;
+   unlink $outcsv;
+   unlink $outcsvt;
    unlink $outvrt;
-   unlink $outdbf;
    print "\n Output shapefile file generated: $final\n\n";
 } else {
    print "\n Shapefile not generated...something's wrong\n\n";
